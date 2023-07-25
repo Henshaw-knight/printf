9e44fa1ab1b7cae46f116ab68db14d4663d2e4b0
Henshaw <josephhensh2000@gmail.com>
2023-07-25 13:52:29 -0700

    Update file

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
 int _printf(const char *format, ...)
 {
 	va_list arguments;
-	int count = 0, struct_index = 0, ret_value = 0, i = 0;
+	int count = 0, struct_index = 0, ret_value = 0, i = 0, n;
 	s_print specs[] = {
 		{"c", print_char},
 		{"s", handle_string},
@@ -18,7 +18,6 @@ int _printf(const char *format, ...)
 	};
 
 	va_start(arguments, format);
-
 	if (format == NULL || !format[0])
 		return (-1);
 	while (format[i])
@@ -30,20 +29,29 @@ int _printf(const char *format, ...)
 				if (format[i + 1] == *specs[struct_index].arg_type)
 				{
 					ret_value = specs[struct_index].f(arguments);
-					if (ret_value == -1)
-						return (-1);
 					count += ret_value;
 				}
 				struct_index++;
 			}
+			if (format[i + 1] == 'd' || format[i + 1] == 'i')
+			{
+				n = va_arg(arguments, int);
+				count += handle_number(n);
+			}
 			if (format[i + 1] == '%')
 			{
-				_putchar(format);
+				_putchar('%');
 				count++;
 			}
 			i++;
 		}
-		
-		
+		else
+		{
+			_putchar(format[i]);
+			i++;
+			count++;
+		}
 	}
+	va_end(arguments);
+	return (count);
 }
